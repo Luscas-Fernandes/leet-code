d38254f627f43605ade1484e8e3c5dc734dd850d
Lucas Fernandes <lfaa2057@gmail.com>
2024-04-20 19:39:11 -0300

    roman to integer

--- 13-romantoi.py
+++ 13-romantoi.py
@@ -1,3 +1,28 @@
 class Solution:
     def romanToInt(self, s):
-        
+        roman_n = {
+            'I' : 1,
+            'V' : 5,
+            'X' : 10,
+            'L' : 50,
+            'C' : 100,
+            'D' : 500,
+            'M' : 1000
+        }
+
+        total = 0
+        last_vis = 0
+
+        for alg in reversed(s):
+            if last_vis > roman_n.get(alg):
+                total -= roman_n.get(alg)
+            else:
+                total += roman_n.get(alg)
+                
+            last_vis = roman_n.get(alg)
+
+        return total
+
+solution_instance = Solution()
+
+print(solution_instance.romanToInt("MCMXCIV"))
