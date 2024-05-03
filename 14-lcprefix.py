62a1e323f7b3d8308166be255dda745c1c4f60c5
Lucas Fernandes <lfaa2057@gmail.com>
2024-05-03 09:53:46 -0300

    done: 14

--- 14-lcprefix.py
+++ 14-lcprefix.py
@@ -1,3 +1,27 @@
 class Solution:
-    def longestCommonPrefix(self, strs: list[str]):
-        pass
+    def longestCommonPrefix(self, strs: list[str]): # rever esse 14
+        if not strs:
+            return ""
+
+        lcp = ""
+        strs_min_length = len(min(strs, key=len))
+
+        for char in range(strs_min_length):
+            charColumn = strs[0][char]
+            for palavra in strs:
+                if charColumn != palavra[char]:
+                    return lcp
+            lcp += charColumn
+
+        return lcp
+            
+        """ 
+            len(min(strs, key=len)
+            corta as outras strings para o tamanho 
+            menor da string minima (Já que é longest common PREFIX), 
+            não substring
+        """
+
+solution_instance = Solution()
+
+print(solution_instance.longestCommonPrefix(["flower","flow","flight"]))
