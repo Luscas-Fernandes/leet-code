654c08b433e8f0ff8cbd3db3c4789c3776c7df6d
luscas-fernandes <lfaa2057@gmail.com>
2026-03-13 17:24:46 -0300

    heap

---
+++ leet codes/23.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        heap = []
+
+        def push_node(heap, node):
+            if node:
+                heapq.heappush(heap, (node.val, id(node), node))
+
+
+        for node in lists:
+            push_node(heap, node)
+
+        dummy = ListNode()
+        current = dummy
+
+        while heap:
+            _, _, node = heapq.heappop(heap)
+            current.next = node
+            current = current.next
+            if node.next:
+                push_node(heap, node.next)
+        
+        return dummy.next
